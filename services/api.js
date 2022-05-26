7c116ace7ec1c9d25ca79073c4a2a2589a4b5e51
Alexandre Bourdeaud'Hui <a.bourdeaudhui@mamoka.com>
2022-05-26 12:38:42 +0200

    Completed ApiService and AuthService

--- services/api.js
+++ services/api.js
@@ -1,1 +1,82 @@
-export class ApiService {}
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { HttpClient } from 'aurelia-http-client';
+
+@inject(Router)
+export class ApiService {
+  constructor(router) {
+    this.router = router;
+    this.isKamaji = true;
+    this.language = null;
+    
+    this.storage = ENVIRONMENT.APP_STORAGE && window[ENVIRONMENT.APP_STORAGE] ? window[ENVIRONMENT.APP_STORAGE] : localStorage;
+    this.baseUrl = this.storage.getItem('APP_KAMAJI_BASE_URL') || ENVIRONMENT.APP_KAMAJI_BASE_URL || this.storage.getItem('APP_API_BASE_URL') || ENVIRONMENT.APP_API_BASE_URL;
+    
+    this.auth = null; // If needed, this will be referenced by the AuthService class itself
+    
+    this.client = new HttpClient();
+    this.client.configure(x => {
+      x.withBaseUrl(this.baseUrl);
+      x.withInterceptor({
+        request: msg => {
+          if (this.auth && this.auth.accessToken && !msg.url.startsWith(this.baseUrl + this.auth.endpoints.refresh) && !msg.headers.has('Authorization')) {
+            msg.headers.add('Authorization', 'Bearer ' + this.auth.accessToken);
+          }
+          if (this.language && !msg.headers.has('Accept-Language')) {
+            msg.headers.add('Accept-Language', this.language);
+          }
+          return msg;
+        },
+        response: msg => {
+          if (String(msg.statusCode) !== '204' && msg.responseType === 'json' && typeof msg.response === 'string') {
+            msg.response = JSON.parse(msg.response);
+          }
+          return msg;
+        },
+        responseError: msg => {
+          if (msg.statusCode === 401 && this.auth && this.auth.refreshToken && !msg.requestMessage.url.startsWith(this.baseUrl + this.auth.endpoints.refresh)) {
+           return this.auth.refresh().then(success => {
+              msg.requestMessage.headers.add('Authorization', 'Bearer ' + this.auth.accessToken);
+              return this.client.send(msg.requestMessage);
+            }, error => {
+              this.auth.logout(this.router?.currentInstruction?.config?.name || null);
+              try { 
+                if (this.router.routes.find(x => x.name === 'logout' || x.href === 'logout')) this.router.navigateToRoute('logout');
+                else if (this.router.routes.find(x => x.name === 'login' || x.href === 'login')) this.router.navigateToRoute('login');
+              } catch(e) {};
+              return error;
+            });
+          }
+          return Promise.reject(msg);
+        }
+      });
+    });
+  }
+
+  get(endpoint, params) {
+    let queryString = (typeof params === 'object') ? '?' + this.buildQueryString(params) : '';
+    return this.client.get(this.baseUrl + endpoint + queryString);
+  }
+
+  post(endpoint, data) {
+    return this.client.post(this.baseUrl + endpoint, data);
+  }
+
+  put(endpoint, data) {
+    return this.client.put(this.baseUrl + endpoint, data);
+  }
+
+  patch(endpoint, data) {
+    return this.client.patch(this.baseUrl + endpoint, data);
+  }
+
+  delete(endpoint) {
+    return this.client.delete(this.baseUrl + endpoint);
+  }
+
+  buildQueryString(params) {
+    let query = [];
+    for (let [key, value] of Object.entries(params)) query.push(`${key}=` + encodeURIComponent(value));
+    return query.join('&');
+  }
+}
--- services/auth.js
+++ services/auth.js
@@ -1,1 +1,109 @@
-export class AuthService {}
+import { inject } from 'aurelia-framework';
+import { ApiService } from 'aurelia-components';
+
+@inject(ApiService)
+export class AuthService {
+  constructor(api) {
+    this.api = api;
+    this.api.auth = this.api.auth || this;
+    this.storage = ENVIRONMENT.APP_STORAGE && window[ENVIRONMENT.APP_STORAGE] ? window[ENVIRONMENT.APP_STORAGE] : localStorage;
+    this.endpoints = { authentication: 'auth', service: 'auth/service', refresh: 'auth/refresh', authorization: 'auth/sua' };
+  }
+
+  get accessToken() {
+    return this.storage.getItem('access_token');
+  }
+  set accessToken(value) {
+    if (value) this.storage.setItem('access_token', value);
+    else this.storage.removeItem('access_token');
+  }
+
+  get refreshToken() {
+    return this.storage.getItem('refresh_token');
+  }
+  set refreshToken(value) {
+    if (value) this.storage.setItem('refresh_token', value);
+    else this.storage.removeItem('refresh_token');
+  }
+
+  get authorization() {
+    return JSON.parse(this.storage.getItem('authorization'));
+  }
+  set authorization(value) {
+    if (value) this.storage.setItem('authorization', JSON.stringify(value));
+    else this.storage.removeItem('authorization');
+  }
+
+  reset() {
+    this.accessToken = null;
+    this.refreshToken = null;
+    this.authorization = null;
+  }
+
+  login(username, password) {
+    this.reset();
+    return new Promise((resolve, reject) => {
+      this.api.post(this.endpoints.authentication, { username, password, grant_type: 'password' }).then(xhr => {
+        this.accessToken = xhr.response.access_token;
+        this.refreshToken = xhr.response.refresh_token;
+        resolve(this);
+      }).catch(error => { reject(error); });
+    }).catch(error => {
+      console.error('Authentication promise error', error);
+      throw error;
+    });
+  }
+
+  service(username, password) {
+    this.reset();
+    return new Promise((resolve, reject) => {
+      this.api.post(this.endpoints.authentication, { username, password, grant_type: 'password' }).then(xhr => {
+        if (!xhr.response.service_token) return reject('Service token missing');
+        this.api.post(`${this.endpoints.service}/${xhr.response.service_token}`).then(xhr => {
+          this.accessToken = xhr.response.access_token;
+          this.refreshToken = xhr.response.refresh_token;
+          resolve(this);
+        }).catch(error => { reject(error); });
+      }).catch(error => { reject(error); });
+    }).catch(error => {
+      console.error('Authentication promise error', error);
+      throw error;
+    });
+  }
+
+  authorize(sid, suat) {
+    this.reset();
+    return new Promise((resolve, reject) => {
+      this.api.post(this.endpoints.authorization, { sid, suat }).then(xhr => {
+        this.accessToken = xhr.response.access_token;
+        this.refreshToken = xhr.response.refresh_token;
+        this.authorization = { sid, suat }
+        resolve(this);
+      }).catch(error => { reject(error); });
+    }).catch(error => {
+      console.error('Authorization promise error', error);
+      throw error;
+    });
+  }
+
+  refresh() {
+    if (this._refreshPromise) return this._refreshPromise;
+    return this._refreshPromise = new Promise((resolve, reject) => {
+      this.api.post(`${this.endpoints.refresh}/${this.refreshToken}`).then(xhr => {
+        this.accessToken = xhr.response.access_token;
+        this.refreshToken = xhr.response.refresh_token;
+        resolve(this);
+      }).catch(error => { reject(error); });
+    }).catch(error => {
+      console.error('Refresh promise error', error);
+      throw error;
+    }).finally(() => {
+      this._refreshPromise = null;
+    });
+  }
+
+  logout(referrer = null) {
+    this.reset();
+    if (referrer) this.referrer = referrer;
+  }
+}
