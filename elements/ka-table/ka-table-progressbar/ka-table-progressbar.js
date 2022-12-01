cdebf8bd662b16d7ea74b60d16901b17e0683fc4
Alexandre Bourdeaud'Hui <a.bourdeaudhui@mamoka.com>
2022-12-01 14:36:34 +0100

    Improvements on table related stuff

--- elements/ka-table/ka-table-progressbar/ka-table-progressbar.js
+++ elements/ka-table/ka-table-progressbar/ka-table-progressbar.js
@@ -1,6 +1,6 @@
-import { inject, customElement } from "aurelia-framework";
+import { inject, customElement } from 'aurelia-framework';
 
-@customElement("ka-table-progressbar")
+@customElement('ka-table-progressbar')
 @inject(Element)
 export class KaTableProgressbar {
   constructor(element) {
@@ -9,24 +9,24 @@ export class KaTableProgressbar {
   bind(bindingContext) {
     this.interface = bindingContext.interface || null;
     if (!this.interface) {
-      console.error("ka-table-progressbar: missing table interface!");
+      console.error('ka-table-progressbar: missing table interface!');
       return;
     } else if (!this.interface.uuid) {
-      console.error("ka-table-progressbar: cannot bind to table interface!");
+      console.error('ka-table-progressbar: cannot bind to table interface!');
       return;
     }
     this.uuid = `ka-table-progressbar-${this.interface.uuid}`;
     this.element.id = this.uuid;
-    this.element.classList.add("visible");
+    this.element.classList.add('visible');
 
-    this.interface.events.addEventListener("load", () => {
-      this.element.classList.add("visible");
+    this.interface.events.addEventListener('load', () => {
+      this.element.classList.add('visible');
     });
-    this.interface.events.addEventListener("loadSuccess", () => {
-      this.element.classList.remove("visible");
+    this.interface.events.addEventListener('loadSuccess', () => {
+      this.element.classList.remove('visible');
     });
-    this.interface.events.addEventListener("loadFailure", () => {
-      this.element.classList.remove("visible");
+    this.interface.events.addEventListener('loadFailure', () => {
+      this.element.classList.remove('visible');
     });
   }
 }
--- elements/ka-table/ka-table/ka-table.js
+++ elements/ka-table/ka-table/ka-table.js
@@ -1,52 +1,52 @@
-import { inject, customElement, bindable, BindingEngine } from "aurelia-framework";
+import { inject, customElement, bindable, BindingEngine } from 'aurelia-framework';
 
-@customElement("ka-table")
+@customElement('ka-table')
 @inject(Element, BindingEngine)
 export class KaTable {
   @bindable() height = 'full';
+  observers = [];
   constructor(element, binding) {
     this.element = element;
     this.binding = binding;
   }
   bind(bindingContext) {
-    this.interface = bindingContext && bindingContext.constructor?.name === "TableInterface"? bindingContext: null;
+    // Resolve TableInterface binding
+    this.interface = bindingContext && bindingContext.constructor?.name === 'TableInterface'? bindingContext: null;
     if (!this.interface) {
-      console.error("ka-table: missing table interface!");
+      console.warn();('ka-table: missing table interface!');
       return;
     } else if (!this.interface.uuid) {
-      console.error("ka-table: cannot bind to table interface!");
+      console.warn('ka-table: cannot bind to table interface!');
       return;
     }
-    // Observe change on  this.interface.isLoading
-    this.binding
-      .expressionObserver(this, 'interface.isLoading')
-      .subscribe((value) => {
-        console.debug('ka-table: observable interface.isLoading', value);
-        if (value) this.element.classList.add('isLoading')
-        else this.element.classList.remove('isLoading');
-      });
-
     this.uuid = `ka-table-${this.interface.uuid}`;
     this.element.id = this.uuid;
+    // Observe change on this.interface.isLoading
+    this.observers.push(this.binding.expressionObserver(this, 'interface.isLoading').subscribe((value) => {
+      console.debug('ka-table: observable interface.isLoading', value);
+      if (value) this.element.classList.add('isLoading')
+      else this.element.classList.remove('isLoading');
+    }));
   }
   attached() {
     const resizeHandler = () => {
       if (this.height === 'full') {
         let element = document.querySelector(`#${this.uuid}`);
         if (!element) return;
-        element.style.height = "0px";
+        element.style.height = '0px';
         element.style.height = parseInt(window.innerHeight - element.getBoundingClientRect().top) + 'px';
       }
     };
     window[`resize-handler-${this.uuid}`] = resizeHandler;
-    window.addEventListener("resize", window[`resize-handler-${this.uuid}`]);
+    window.addEventListener('resize', window[`resize-handler-${this.uuid}`]);
     setTimeout(() => {
       resizeHandler();
     }, 1000);
   }
 
   detached() {
-    window.removeEventListener("resize", window[`resize-handler-${this.uuid}`]);
+    window.removeEventListener('resize', window[`resize-handler-${this.uuid}`]);
     delete window[`resize-handler-${this.uuid}`];
+    for (let observer of this.observers) observer.dispose();
   }
 }
