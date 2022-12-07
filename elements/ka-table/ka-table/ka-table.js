5fe57c4fd682a73d6adc97ce8d20d6b44f74aadf
Alexandre Bourdeaud'Hui <a.bourdeaudhui@mamoka.com>
2022-12-07 10:09:33 +0100

    Fix on ka-table resize/scroll listeners

--- elements/ka-table/ka-table/ka-table.js
+++ elements/ka-table/ka-table/ka-table.js
@@ -23,12 +23,10 @@ export class KaTable {
     this.element.id = this.uuid;
     // Observe change on this.interface.isLoading
     this.observers.push(this.binding.expressionObserver(this, 'interface.isLoading').subscribe((value) => {
-      console.debug('ka-table: observable interface.isLoading', value);
       if (value) this.element.classList.add('isLoading')
       else this.element.classList.remove('isLoading');
     }));
     this.interface.events.addEventListener('loadSuccess', () => {
-      console.warn('ka-table: loadSuccess!');
       setTimeout(() => {
         try { window[`resize-handler-${this.uuid}`](); } catch (error) { console.warn('ka-table: cannot run resize handler on load!'); }
       }, 0);
@@ -45,23 +43,21 @@ export class KaTable {
       }
       element.querySelectorAll('tbody td.ka-table-row-tools:last-child').forEach(function (element) {
         var floater = element.querySelector('.floater');
+        var table = element.closest('ka-table');
         if (!floater) return;
         var h = element.getBoundingClientRect().height;
         var w = floater.getBoundingClientRect().width;
-        if (floater && h && w) {
+        if (table && floater && h && w) {
+          table.scrollLeft = 0;
           floater.classList.add('float');
-          floater.style.transform = `translateX(${element.closest('ka-table').scrollLeft}px)`;
           floater.style.height = h + 'px';
-          element.style.minWidth = w + 'px';
+          element.style.minWidth = element.style.width = w + 'px';
         }
       });
     };
     window[`resize-handler-${this.uuid}`] = resizeHandler;
     window.addEventListener('resize', window[`resize-handler-${this.uuid}`]);
-    /* setTimeout(() => {
-      resizeHandler();
-    }, 1000); */
-
+    // Scroll event listener
     this.element.addEventListener('scroll', event => {
       event.target.querySelectorAll('.floater.float').forEach(floater => {
         floater.style.transform = `translateX(${event.target.scrollLeft}px)`;
